f62e0d4064d576cf40437ecdcebc7885f823321e
unknown <weichangk>
2021-06-01 16:28:56 +0800

    添加修改本地时间方法

--- Util/Time/Time.cs
+++ Util/Time/Time.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Runtime.InteropServices;
 
 namespace Util
 {
@@ -543,5 +544,42 @@ namespace Util
             dateTime = dateTime + time;
             return dateTime;
         }
+
+        #region 更新本地时间
+        [DllImport("Kernel32.dll")]
+        private extern static void GetSystemTime(ref SYSTEMTIME lpSystemTime);
+
+        [DllImport("Kernel32.dll")]
+        private extern static uint SetLocalTime(ref SYSTEMTIME lpSystemTime);
+        [StructLayout(LayoutKind.Sequential)]
+        private struct SYSTEMTIME
+        {
+            public ushort wYear;
+            public ushort wMonth;
+            public ushort wDayOfWeek;
+            public ushort wDay;
+            public ushort wHour;
+            public ushort wMinute;
+            public ushort wSecond;
+            public ushort wMilliseconds;
+        }
+        /// <summary>
+        /// 将本地时间服务器时间同步
+        /// </summary>
+        /// <param name="SqlServerTime">时间</param>
+        public static void SynchroTime(DateTime ServerTime)
+        {
+            //服务器时间
+            SYSTEMTIME st = new SYSTEMTIME();
+            st.wYear = Convert.ToUInt16(ServerTime.Year);
+            st.wMonth = Convert.ToUInt16(ServerTime.Month);
+            st.wDay = Convert.ToUInt16(ServerTime.Day);
+            st.wHour = Convert.ToUInt16(ServerTime.Hour);
+            st.wMilliseconds = Convert.ToUInt16(ServerTime.Millisecond);
+            st.wMinute = Convert.ToUInt16(ServerTime.Minute);
+            st.wSecond = Convert.ToUInt16(ServerTime.Second);
+            SetLocalTime(ref st);
+        }
+        #endregion
     }
 }
