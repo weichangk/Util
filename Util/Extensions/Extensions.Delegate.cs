79a483e6a605c21d7d2bc7ea183e870d9c4004d0
unknown <weichangk>
2021-06-29 11:31:49 +0800

    Extensions

---
+++ Util/Extensions/Extensions.Delegate.cs
@@ -0,0 +1,101 @@
+﻿namespace Util
+{
+    /// <summary>
+    /// 委托扩展
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// 带ref参数的Action
+        /// </summary>
+        /// <typeparam name="TRef"></typeparam>
+        /// <param name="rf"></param>
+        public delegate void RefAction<TRef>(ref TRef rf);
+
+        /// <summary>
+        /// 带ref参数的Action
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TRef"></typeparam>
+        /// <param name="t"></param>
+        /// <param name="rf"></param>
+        public delegate void RefAction<T, TRef>(T t, ref TRef rf);
+
+        /// <summary>
+        /// 带ref参数的Action
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="T1"></typeparam>
+        /// <typeparam name="TRef"></typeparam>
+        /// <param name="t"></param>
+        /// <param name="t1"></param>
+        /// <param name="rf"></param>
+        public delegate void RefAction<T, T1, TRef>(T t, T1 t1, ref TRef rf);
+
+        /// <summary>
+        /// 带ref参数的Func委托
+        /// </summary>
+        /// <typeparam name="TRef"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="rf"></param>
+        /// <returns></returns>
+        public delegate TResult RefFunc<TRef, out TResult>(ref TRef rf);
+
+        /// <summary>
+        /// 带ref参数的Func委托
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TRef"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="t"></param>
+        /// <param name="rf"></param>
+        /// <returns></returns>
+        public delegate TResult RefFunc<T, TRef, out TResult>(T t, ref TRef rf);
+
+        /// <summary>
+        /// 带ref参数的Func委托
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="T1"></typeparam>
+        /// <typeparam name="TRef"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="t"></param>
+        /// <param name="t1"></param>
+        /// <param name="rf"></param>
+        /// <returns></returns>
+        public delegate TResult RefFunc<T, T1, TRef, out TResult>(T t, T1 t1, ref TRef rf);
+
+        /// <summary>
+        /// 带out参数和返回值的Func委托
+        /// </summary>
+        /// <typeparam name="TOut"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="ot"></param>
+        /// <returns></returns>
+        public delegate TResult OutFunc<TOut, out TResult>(out TOut ot);
+
+        /// <summary>
+        /// 带out参数和返回值的Func委托
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TOut"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="t"></param>
+        /// <param name="ot"></param>
+        /// <returns></returns>
+        public delegate TResult OutFunc<T, TOut, out TResult>(T t, out TOut ot);
+
+        /// <summary>
+        /// 带out参数和返回值的Func委托
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="T1"></typeparam>
+        /// <typeparam name="TOut"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="t"></param>
+        /// <param name="t1"></param>
+        /// <param name="ot"></param>
+        /// <returns></returns>
+        public delegate TResult OutFunc<T, T1, TOut, out TResult>(T t, T1 t1, out TOut ot);
+    }
+}
---
+++ Util/Extensions/Extensions.String.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+
+namespace Util
+{
+    /// <summary>
+    /// 字符串扩展
+    /// </summary>
+    public static partial class Extensions
+    {
+        public static string AMOUNT_FORMAT = "f2";
+        /// <summary>
+        /// HasValue
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool HasValue(this string s)
+        {
+            return !string.IsNullOrEmpty(s);
+        }
+
+        /// <summary>
+        /// 是否包含元素
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="element"></param>
+        /// <param name="comparisonType"></param>
+        /// <returns></returns>
+        public static bool Contains(this string[] array, string element, StringComparison comparisonType)
+        {
+            if (array == null || array.Length == 0) return false;
+
+            return array.Any(f => f.HasValue() && f.Equals(element, comparisonType));
+        }
+
+        /// <summary>
+        /// ToAmountString
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string ToAmountString(this decimal s)
+        {
+            return s.ToString("f2");
+        }
+
+        /// <summary>
+        /// ToShortAmountString
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string ToCutAmountString(this decimal s)
+        {
+            if (Math.Floor(s) == s)
+                return s.ToString("f0");
+            else if (Math.Floor(s * 10) == (s * 10))
+                return s.ToString("f1");
+            else if (Math.Floor(s * 100) == (s * 100))
+                return s.ToString("f2");
+            else if (Math.Floor(s * 1000) == (s * 1000))
+                return s.ToString("f3");
+            return s.ToString("f2");
+        }
+
+        /// <summary>
+        /// 截取左边指定长度字符串
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static string Left(this string s, int length)
+        {
+            return !string.IsNullOrEmpty(s) && s.Length > length ? s.Substring(0, length) : s;
+        }
+
+        /// <summary>
+        /// 截取右边指定长度字符串
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static string Right(this string s, int length)
+        {
+            return !string.IsNullOrEmpty(s) && s.Length > length ? s.Substring(s.Length - length, length) : s;
+        }
+
+        /// <summary>
+        /// 获取字符串字节长度(中文2个字符)
+        /// </summary>
+        /// <param name="strContent"></param>
+        /// <returns></returns>
+        public static int ByteLength(this string s)
+        {
+            if (!s.HasValue()) return 0;
+            return System.Text.Encoding.Default.GetByteCount(s);
+        }
+    }
+}
